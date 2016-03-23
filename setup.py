3aa75140b63025039c55a7908f886f169f02758b
Bryn Keller <xoltar@xoltar.org>
2016-03-23 15:52:20 -0700

    Simple setup.py that is suitable for use with pybind11, and will include any packages in the toolkit folder

--- setup.py
+++ setup.py
@@ -1,1 +1,51 @@
-# TODO: The whole thing!
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+import sys
+import os.path
+ext_modules = [
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+    c_opts = {
+        'msvc': ['/EHsc'],
+        'unix': ['-std=c++11'],
+    }
+
+    if sys.platform == 'darwin':
+        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+        build_ext.build_extensions(self)
+
+setup(
+    name='toolkit',
+    version='0.0.1',
+    author='Princeton Neuroscience Institute and Intel Corporation',
+    author_email='bryn.keller@intel.com',
+    url='https://github.com/IntelPNI/toolkit',
+    description='Scalable algorithms for advanced fMRI analysis',
+    license = 'Apache 2',
+    keywords='neuroscience, algorithm, fMRI, distributed, scalable',
+    long_description=long_description,
+    ext_modules=ext_modules,
+    # install_requires=['pybind11'],
+    cmdclass={'build_ext': BuildExt},
+    # py_modules = ['phat'],
+    packages = find_packages(exclude = ['doc', 'test'])
+ )
+
+
+
+
+
