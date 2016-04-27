a6a99d929595c2e5be04b25bdd824915f79f92cc
Mihai Capotă <mihai@mihaic.ro>
2016-04-26 19:43:48 -0700

    Add sphinx-apidoc to docs Makefile

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -276,7 +276,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   (master_doc, 'toolkit', 'toolkit Documentation',
-   author, 'toolkit', 'One line description of project.',
+   author, 'toolkit', 'Intel-PNI Neuroscience Toolkit for Python.',
    'Miscellaneous'),
 ]
 
--- docs/index.rst
+++ docs/index.rst
@@ -3,19 +3,14 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to toolkit's documentation!
-===================================
+toolkit documentation
+=====================
 
 Contents:
 
 .. toctree::
-   :maxdepth: 2
 
-.. automodule:: toolkit
-   :members:
-
-.. automodule:: toolkit.srm
-   :members:
+   modules
 
 Indices and tables
 ==================
@@ -23,4 +18,3 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
-
