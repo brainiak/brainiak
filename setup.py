15b2264be1b4602793285019267086f1b0b74f02
xia.zhu <xia.zhu@intel.com>
2016-06-16 13:36:19 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from setuptools import setup, find_packages, Extension
-from setuptools.command.build_ext import build_ext
-from distutils.version import LooseVersion
-
-import sys
 import os
 import re
+import sys
 import platform
 import subprocess
 
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
 
 assert sys.version_info >= (3, 4), (
     "Please use Python version 3.4 or higher, "
@@ -21,22 +20,14 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 
-class BuildExt(build_ext):
-    try:
-        out = subprocess.check_output(['cmake', '--version'])
-    except OSError:
-        raise RuntimeError(
-            "CMake must be installed to build the following extensions: " +
-            ", ".join(
-                e.name for e in self.extensions))
+class CMakeExtension(Extension):
 
-    if platform.system() == "Windows":
-        cmake_version = LooseVersion(
-            re.search(
-                r'version\s*([\d.]+)',
-                out.decode()).group(1))
-        if cmake_version < '3.1.0':
-            raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
 
     """A custom build extension for adding compiler-specific options."""
     c_opts = {
@@ -46,48 +37,62 @@ class BuildExt(build_ext):
     if sys.platform == 'darwin':
         c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
 
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
+
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: " +
+                ", ".join(
+                    e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(
+                re.search(
+                    r'version\s*([\d.]+)',
+                    out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
         for ext in self.extensions:
-            ext.extra_compile_args = opts
-            extdir = os.path.abspath(
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(
             os.path.dirname(
                 self.get_ext_fullpath(
                     ext.name)))
-            cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                          '-DPYTHON_EXECUTABLE=' + sys.executable]
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
 
-            cfg = 'Debug' if self.debug else 'Release'
-            build_args = ['--config', cfg]
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
 
-            if platform.system() == "Windows":
-                cmake_args += [
-                    '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(),
-                                                                    extdir)]
-                if sys.maxsize > 2**32:
-                    cmake_args += ['-A', 'x64']
-                build_args += ['--', '/m']
-            else:
-                cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-                build_args += ['--', '-j2']
+        if platform.system() == "Windows":
+            cmake_args += [
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(),
+                                                                extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
 
-            env = os.environ.copy()
-            env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-                env.get(
-                    'CXXFLAGS',
-                    ''),
-                self.distribution.get_version())
-            if not os.path.exists(self.build_temp):
-                os.makedirs(self.build_temp)
-            subprocess.check_call(
-                ['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp,
-                env=env)
-            subprocess.check_call(
-                ['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
-
-
-        build_ext.build_extensions(self)
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get(
+                'CXXFLAGS',
+                ''),
+            self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(
+            ['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp,
+            env=env)
+        subprocess.check_call(
+            ['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 setup(
     name='brainiak',
@@ -105,6 +110,7 @@ setup(
     license='Apache 2',
     keywords='neuroscience, algorithm, fMRI, distributed, scalable',
     long_description=long_description,
-    cmdclass={'build_ext': BuildExt},
-    packages=find_packages(exclude=['doc', 'test']),
+    ext_modules=[CMakeExtension('tfa_extension')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    packages=find_packages(exclude=['doc', 'test', 'pybind11']),
 )
