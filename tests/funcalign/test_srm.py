2077b56f590e48db5838584fece0ac8d620f3af3
yshin1 <yshin@princeton.edu>
2016-07-24 20:11:13 -0400

    Add transform to SRM (#77)
    modified:   examples/funcalign/srm_image_prediction_example.py

--- tests/funcalign/test_srm.py
+++ tests/funcalign/test_srm.py
@@ -11,6 +11,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from sklearn.utils.validation import NotFittedError
+
+
 def test_can_instantiate():
     import brainiak.funcalign.srm
     s = brainiak.funcalign.srm.SRM()
@@ -43,12 +46,19 @@ def test_can_instantiate():
     W.append(Q)
     X.append(Q.dot(S) + 0.1*np.random.random((voxels, samples)))
 
-    # Check that does NOT run with 1 subject
+    # Check that transform does NOT run before fitting the model
+    try:
+        s.transform(X)
+        assert True, "Success transforming before fitting the model!"
+    except NotFittedError:
+        print("Caught Exception number 1: transforming before fitting the model")
+
+    # Check that it does NOT run with 1 subject
     try:
         s.fit(X)
         assert True, "Success running SRM with one subject!"
     except ValueError:
-        print("Catched Exception number 1: running SRM with 1 subject")
+        print("Caught Exception number 1: running SRM with 1 subject")
 
     for subject in range(1, subjects):
         Q, R = np.linalg.qr(np.random.random((voxels, features)))
@@ -67,6 +77,13 @@ def test_can_instantiate():
         assert s.w_[subject].shape[1] == features, "Invalid computation of SRM! (wrong # features in W)"
     assert s.s_.shape[0] == features, "Invalid computation of SRM! (wrong # features in S)"
     assert s.s_.shape[1] == samples, "Invalid computation of SRM! (wrong # samples in S)"
+
+    # Check that it does NOT run with non-matcing number of subjects
+    try:
+        s.transform(X[1])
+        assert True, "Success transforming with non-matching number of subjects"
+    except ValueError:
+        print("Caught Exception number 2: transforming with non-matching number of subjects")
 
     # Check that it does not run without enough samples (TRs).
     try:
