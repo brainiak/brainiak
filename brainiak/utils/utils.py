e84cd76ecade13957fce6d52db13db887036ab86
lcnature <lcnature@github.com>
2016-09-07 21:49:42 -0400

    Added a test for read_design in utils.py. Corrected the docstring error (hopefully)

--- brainiak/utils/utils.py
+++ brainiak/utils/utils.py
@@ -24,12 +24,14 @@ Some utility functions that can be used by different algorithms
 def from_tri_2_sym(tri, dim):
     """convert a upper triangular matrix in 1D format
        to 2D symmetric matrix
+
     Parameters
     ----------
     tri: 1D array
         Contains elements of upper triangular matrix
     dim : int
         The dimension of target matrix.
+
     Returns
     -------
     symm : 2D array
@@ -43,16 +45,17 @@ def from_tri_2_sym(tri, dim):
 def from_sym_2_tri(symm):
     """convert a 2D symmetric matrix to an upper
        triangular matrix in 1D format
+
     Parameters
     ----------
     symm : 2D array
           Symmetric matrix
+
     Returns
     -------
     tri: 1D array
           Contains elements of upper triangular matrix
     """
-
     inds = np.triu_indices_from(symm)
     tri = symm[inds]
     return tri
@@ -60,13 +63,16 @@ def from_sym_2_tri(symm):
 
 def fast_inv(a):
     """to invert a 2D matrix
+
     Parameters
     ----------
     a : 2D array
+
     Returns
     -------
     inva: 2D array
          inverse of input matrix a
+
     Raises
     -------
     LinAlgError
