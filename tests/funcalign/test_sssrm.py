a008581bc684ad549ccdb759147000a120a222f0
Javier Turek <javier.turek@intel.com>
2016-09-20 14:58:17 -0700

    Remove verbose flag from test

--- tests/funcalign/test_sssrm.py
+++ tests/funcalign/test_sssrm.py
@@ -31,7 +31,7 @@ def test_sssrm():
     features = 3
     n_labels = 4
 
-    model = brainiak.funcalign.sssrm.SSSRM(verbose=True, n_iter=5, features=features, gamma=10.0, cost=0.1)
+    model = brainiak.funcalign.sssrm.SSSRM(n_iter=5, features=features, gamma=10.0, cost=0.1)
     assert model, "Invalid SSSRM instance!"
 
     # Create a Shared response S with K = 3
@@ -93,14 +93,14 @@ def test_sssrm():
     print("Test: running SSSRM with 1 subject (classif.)")
 
     # Check that cost is in (0,1) range
-    model_bad = brainiak.funcalign.sssrm.SSSRM(verbose=False, n_iter=1, features=features, gamma=10.0, cost=1.5)
+    model_bad = brainiak.funcalign.sssrm.SSSRM(n_iter=1, features=features, gamma=10.0, cost=1.5)
     assert model_bad, "Invalid SSSRM instance!"
     with pytest.raises(ValueError) as excinfo:
         model_bad.fit(X, y, Z)
     print("Test: running SSSRM with wrong cost")
 
     # Check that gamma is positive
-    model_bad = brainiak.funcalign.sssrm.SSSRM(verbose=False, n_iter=1, features=features, gamma=-0.1, cost=0.2)
+    model_bad = brainiak.funcalign.sssrm.SSSRM(n_iter=1, features=features, gamma=-0.1, cost=0.2)
     assert model_bad, "Invalid SSSRM instance!"
     with pytest.raises(ValueError) as excinfo:
         model_bad.fit(X, y, Z)
