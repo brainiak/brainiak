e0103280b4b3bf3c28f316a9f1235aa439d2809b
Javier Turek <javier.turek@intel.com>
2016-09-21 09:50:16 -0700

    Add Deterministic SRM (#102)
    * Added Deterministic Shared Response Model
    
    * Corrected complexity
    
    * Update srm.py
    
    * Typo
    
    * too long documentation line
    
    * trailing white space removed
    
    * Modified documentation and added arxiv link
    
    * Removed unnecessary if's for logging
    
    * Changed the complexity description
    
    * Removed verbose flag from SRM and DetSRM)
    
    * Added orthogonality check
    
    * Corrected SRM test
    
    * Added test for data fit of the model
    
    * remove verbose flag from example
    
    * added relative error
    
    * modify 20 by INFO constant

--- examples/funcalign/srm_image_prediction_example.py
+++ examples/funcalign/srm_image_prediction_example.py
@@ -16,6 +16,7 @@ from scipy.stats import stats
 from sklearn.metrics import confusion_matrix
 from sklearn.svm import NuSVC
 import numpy as np
+import brainiak.funcalign.srm
 
 # Load the input data that contains the movie stimuli for unsupervised training with SRM
 movie_data = scipy.io.loadmat('data/movie_data.mat')
@@ -27,17 +28,17 @@ subjects = len(movie_data)
 
 # Z-score the data
 for subject in range(subjects):
-    movie_data[subject] = stats.zscore(movie_data[subject],axis=1,ddof=1)
+    movie_data[subject] = stats.zscore(movie_data[subject], axis=1, ddof=1)
 
 # Run SRM with the movie data
-import brainiak.funcalign.srm
 help(brainiak.funcalign.srm.SRM)
-srm = brainiak.funcalign.srm.SRM(n_iter=10, features=50, verbose=True)
+srm = brainiak.funcalign.srm.SRM(n_iter=10, features=50)
 srm.fit(movie_data)
 
 # We define a function to present the output of the experiment.
 def plot_confusion_matrix(cm, title="Confusion Matrix"):
-    """Plots a confusion matrix for each subject"""
+    """Plots a confusion matrix for each subject
+    """
     import matplotlib.pyplot as plt
     import math
     plt.figure()
@@ -49,9 +50,9 @@ def plot_confusion_matrix(cm, title="Confusion Matrix"):
     for subject in range(subjects):
         plt.subplot(rows, cols, subject+1)
         plt.imshow(cm[subject], interpolation='nearest', cmap=plt.cm.bone)
-        plt.xticks(np.arange(classes), range(1,classes+1))
-        plt.yticks(np.arange(classes), range(1,classes+1))
-        cbar = plt.colorbar(ticks=[0.0,1.0], shrink=0.6)
+        plt.xticks(np.arange(classes), range(1, classes+1))
+        plt.yticks(np.arange(classes), range(1, classes+1))
+        cbar = plt.colorbar(ticks=[0.0, 1.0], shrink=0.6)
         cbar.set_clim(0.0, 1.0)
         plt.xlabel("Predicted")
         plt.ylabel("True label")
@@ -70,7 +71,7 @@ assert image_data[0].shape[0] == movie_data[0].shape[0], "Number of voxels in mo
 
 # Z-score the image data
 for subject in range(subjects):
-    image_data[subject] = stats.zscore(image_data[subject],axis=1,ddof=1)
+    image_data[subject] = stats.zscore(image_data[subject], axis=1, ddof=1)
 
 # Z-score the shared response data
 image_data_shared = srm.transform(image_data)
@@ -84,7 +85,7 @@ labels = np.squeeze(labels['label'])
 # Run a leave-one-out cross validation with the subjects
 train_labels = np.tile(labels, subjects-1)
 test_labels = labels
-accuracy = np.zeros((subjects))
+accuracy = np.zeros((subjects,))
 cm = [None] * subjects
 for subject in range(subjects):
     # Concatenate the subjects' data for training into one matrix
@@ -113,7 +114,47 @@ for subject in range(subjects):
 
 
 # Plot and print the results
-plot_confusion_matrix(cm, title="Confusion matrices for different test subjects")
-print("The average accuracy among all subjects is {0:f} +/- {1:f}".format(np.mean(accuracy), np.std(accuracy)))
+plot_confusion_matrix(cm, title="Confusion matrices for different test subjects with Probabilistic SRM")
+print("SRM: The average accuracy among all subjects is {0:f} +/- {1:f}".format(np.mean(accuracy), np.std(accuracy)))
 
+# Now we repeat the experiment with the Deterministic SRM
+# Fit the model
+srm = brainiak.funcalign.srm.DetSRM(n_iter=10, features=50)
+srm.fit(movie_data)
 
+# Transform the image stimuli data
+image_data_shared = srm.transform(image_data)
+for subject in range(subjects):
+    image_data_shared[subject] = stats.zscore(image_data_shared[subject], axis=1, ddof=1)
+
+# Run a leave-one-out cross validation with the subjects
+accuracy = np.zeros((subjects,))
+cm = [None] * subjects
+for subject in range(subjects):
+    # Concatenate the subjects' data for training into one matrix
+    train_subjects = list(range(subjects))
+    train_subjects.remove(subject)
+    TRs = image_data_shared[0].shape[1]
+    train_data = np.zeros((image_data_shared[0].shape[0], len(train_labels)))
+    for train_subject in range(len(train_subjects)):
+        start_index = train_subject*TRs
+        end_index = start_index+TRs
+        train_data[:, start_index:end_index] = image_data_shared[train_subjects[train_subject]]
+
+    # Train a Nu-SVM classifier using scikit learn
+    classifier = NuSVC(nu=0.5, kernel='linear')
+    classifier = classifier.fit(train_data.T, train_labels)
+
+    # Predict on the test data
+    predicted_labels = classifier.predict(image_data_shared[subject].T)
+    accuracy[subject] = sum(predicted_labels == test_labels)/float(len(predicted_labels))
+
+    # Create a confusion matrix to see the accuracy of each class
+    cm[subject] = confusion_matrix(test_labels, predicted_labels)
+
+    # Normalize the confusion matrix
+    cm[subject] = cm[subject].astype('float') / cm[subject].sum(axis=1)[:, np.newaxis]
+
+# Plot and print the results
+plot_confusion_matrix(cm, title="Confusion matrices for different test subjects with Deterministic SRM")
+print("Det. SRM: The average accuracy among all subjects is {0:f} +/- {1:f}".format(np.mean(accuracy), np.std(accuracy)))
