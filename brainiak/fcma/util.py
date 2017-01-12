c62f9214160ab04f0fe4cb52255d131c85957943
Yida Wang <yidawa@gmail.com>
2017-01-12 15:28:56 -0800

    add pure high performance correlation computation function

---
+++ brainiak/fcma/util.py
@@ -0,0 +1,53 @@
+#  Copyright 2016 Intel Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""Full Correlation Matrix Analysis (FCMA)
+
+Correlation related high performance routines
+"""
+
+# Authors: Yida Wang
+# (Intel Labs), 2017
+
+import numpy as np
+from . import cython_blas as blas
+from scipy.stats.mstats import zscore
+import math
+
+def normalize_for_correlation(data, axis):
+    shape = data.shape
+    data = zscore(data, axis=axis, ddof=0)
+    # if zscore fails (standard deviation is zero),
+    # set all values to be zero
+    data = np.nan_to_num(data)
+    data = data / math.sqrt(shape[axis])
+    return data
+
+def compute_correlation(component1, component2):
+    [r1, d1] = component1.shape
+    [r2, d2] = component2.shape
+    if d1 != d2:
+        raise ValueError('Dimension discrepancy')
+    # preprocess two components
+    component1 = normalize_for_correlation(component1, 1)
+    component2 = normalize_for_correlation(component2, 1)
+    corr_data = np.empty((r1, r2), dtype=np.float32, order='C')
+    blas.compute_single_matrix_multiplication('T', 'N',
+                                              r1, r2, d1,
+                                              1.0,
+                                              component1, d1,
+                                              component2, d2,
+                                              0.0,
+                                              corr_data,
+                                              r2)
+    return corr_data
---
+++ tests/fcma/test_util.py
@@ -0,0 +1,32 @@
+#  Copyright 2016 Intel Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import numpy as np
+from numpy.random import RandomState
+from brainiak.fcma.util import compute_correlation
+
+# specify the random state to fix the random numbers
+prng = RandomState(1234567890)
+
+def test_correlation_computation():
+    row = 5
+    col = 5
+    mat = prng.rand(row, col).astype(np.float32)
+    corr = compute_correlation(mat, mat)
+    expected_corr = np.corrcoef(mat)
+    assert np.allclose(corr, expected_corr, atol=1e-5), \
+        'high performance correlation computation does not provide correct results'
+
+if __name__ == '__main__':
+    test_correlation_computation()
