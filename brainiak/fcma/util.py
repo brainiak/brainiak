1513e2c0993969efccd3ba073dee9c4cb6acb2ac
Yida Wang <yidawa@gmail.com>
2017-01-12 16:56:56 -0800

    add docstrings

--- brainiak/fcma/util.py
+++ brainiak/fcma/util.py
@@ -25,6 +25,23 @@ from scipy.stats.mstats import zscore
 import math
 
 def normalize_for_correlation(data, axis):
+    """normalize the data before computing correlation
+
+    The data will be z-scored and divided by sqrt(n)
+    along the assigned axis
+
+    Parameters
+    ----------
+    data: 2D array
+
+    axis: int
+        specify which dimension of the data should be normalized
+
+    Returns
+    -------
+    data: 2D array
+        the normalized data
+    """
     shape = data.shape
     data = zscore(data, axis=axis, ddof=0)
     # if zscore fails (standard deviation is zero),
@@ -33,21 +50,42 @@ def normalize_for_correlation(data, axis):
     data = data / math.sqrt(shape[axis])
     return data
 
-def compute_correlation(component1, component2):
-    [r1, d1] = component1.shape
-    [r2, d2] = component2.shape
+def compute_correlation(set1, set2):
+    """compute correlation between two sets
+
+    Correlate the rows of set1 with the rows of set2.
+    If set1 == set2, it is auto-correlation computation
+    resulting in a symmetric correlation matrix.
+    The number of columns MUST agree between set1 and set2
+
+    Parameters
+    ----------
+    set1: 2D array in shape [r1, c]
+        MUST be continuous and row-major
+
+    set2: 2D array in shape [r2, c]
+        MUST be continuous and row-major
+
+    Returns
+    -------
+    corr_data: 2D array in shape [r1, r2]
+    """
+    set1 = set1.astype(np.float32)
+    set2 = set2.astype(np.float32)
+    [r1, d1] = set1.shape
+    [r2, d2] = set2.shape
     if d1 != d2:
         raise ValueError('Dimension discrepancy')
     # preprocess two components
-    component1 = normalize_for_correlation(component1, 1)
-    component2 = normalize_for_correlation(component2, 1)
+    set1 = normalize_for_correlation(set1, 1)
+    set2 = normalize_for_correlation(set2, 1)
     corr_data = np.empty((r1, r2), dtype=np.float32, order='C')
     # blas routine is column-major
     blas.compute_single_matrix_multiplication('T', 'N',
                                               r2, r1, d1,
                                               1.0,
-                                              component2, d2,
-                                              component1, d1,
+                                              set2, d2,
+                                              set1, d1,
                                               0.0,
                                               corr_data,
                                               r2)
