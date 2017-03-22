3c49e8d7e1be2713cc151afe136ea3573d337758
Javier Turek <javier.turek@intel.com>
2017-03-22 16:07:56 -0700

    Improvements and code clean up

--- brainiak/funcalign/dl.py
+++ brainiak/funcalign/dl.py
@@ -35,49 +35,10 @@ from sklearn.utils import assert_all_finite
 from sklearn.utils.validation import NotFittedError
 
 __all__ = [
-    "MDL"
+    "MSDL"
 ]
 
 logger = logging.getLogger(__name__)
-
-def _init_template(data, factors):
-    """Initialize the template spatial map (V) for the MSDL with random orthogonal matrices.
-
-    Parameters
-    ----------
-
-    data : list of 2D arrays, element i has shape=[voxels, samples]
-        Each element in the list contains the fMRI data of one subject.
-
-    factors : int
-        The number of factors in the model.
-
-
-    Returns
-    -------
-
-    v : array, shape=[voxels, factors]
-        The initialized template spatial map :math:`V`.
-
-    Note
-    ----
-
-        This function assumes that the numpy random number generator was
-        initialized.
-
-        Not thread safe.
-    """
-    #v = np.empty(data[0].shape[0], factors)
-    #subjects = len(data)
-    V = np.random.random((data[0].shape[0], factors))
-    # Set Wi to a random orthogonal voxels by features matrix
-    # for subject in range(subjects):
-    #     voxels[subject] = data[subject].shape[0]
-    #     rnd_matrix = np.random.random((voxels[subject], features))
-    #     q, r = np.linalg.qr(rnd_matrix)
-    #     w.append(q)
-
-    return V
 
 
 class MSDL(BaseEstimator, TransformerMixin):
@@ -106,6 +67,18 @@ class MSDL(BaseEstimator, TransformerMixin):
     rand_seed : int, default: 0
         Seed for initializing the random number generator.
 
+    mu : float, default: 1.0
+        Value of the subject-global spatial map matching term.
+
+    lam : float, default: 1.0
+        Value of the regularization parameter.
+
+    kappa : float, default: 0.5
+        Step size for the FISTA algorithm. Should be a value in (0.0, 1.0).
+
+    fista_iter : int, default: 20
+        Number of iterations to run the FISTA algorithm to update the spatial map template.
+
 
     Attributes
     ----------
@@ -119,29 +92,27 @@ class MSDL(BaseEstimator, TransformerMixin):
     Note
     ----
 
-        The number of voxels should be the same between subjects. #TODO: However, the
-        number of samples may be different across subjects.
+        The number of voxels should be the same between subjects.
 
         The Multi-Subject Dictionary Learning is approximated using a
         Block Coordinate Descent (BCD) algorithm proposed in [Varoquaux2011]_.
 
         This is a single node version.
 
-        TODO: The run-time complexity is :math:`O(I (V T K + V K^2))` and the memory
-        complexity is :math:`O(V T)` with I - the number of iterations, V - the
-        sum of voxels from all subjects, T - the number of samples, K - the
-        number of features (typically, :math:`V \\gg T \\gg K`), and N - the
-        number of subjects.
+        The run-time complexity is :math:`O(S (V T K + (V + T) K^2 + K^3 + K I_{V}))` with
+        I - the number of iterations of FISTA, V - the voxels from a subject,
+        T - the number of time samples, K - the number of features/factors (typically, :math:`V \\gg T \\gg K`),
+        and S - the number of subjects.
     """
 
-    def __init__(self, n_iter=10, factors=10, rand_seed=0, mu=1.0, lam=1.0, gamma=1.0, fista_iter=100):
+    def __init__(self, n_iter=10, factors=10, rand_seed=0, mu=1.0, lam=1.0, kappa=0.5, fista_iter=20):
         self.n_iter = n_iter
         self.factors = factors
         self.rand_seed = rand_seed
         self.mu = mu
         self.lam = lam
-        self.gamma = gamma
         self.fista_iter = fista_iter
+        self.kappa = kappa
         return
 
     def fit(self, X, y=None, R=None):
@@ -188,11 +159,14 @@ class MSDL(BaseEstimator, TransformerMixin):
             raise ValueError("Wrong number of voxels in R ({0:d})."
                              .format(len(X)))
 
+        if self.kappa < 0.0 or self.kappa >= 1.0:
+            raise ValueError("Kappa should be in range (0,1)")
+
         # Prepare the laplacian operator for this data
         self.L_ = self._create_laplacian_operator(R)
-        self.max_singularval_L_ = scipy.sparse.linalg.svds(self.L_, 1, return_singular_vectors=False)
-        self.max_singularval_L_ = self.max_singularval_L_[0]
-        print(self.max_singularval_L_)
+        self.max_eigval_L_ = scipy.sparse.linalg.svds(self.L_, k=1, which='LM', return_singular_vectors=False)
+        self.max_eigval_L_ =  (self.max_eigval_L_[0]**2)
+        logger.info("Lipschitz Constant of L ",self.max_eigval_L_)
 
         # Run MSDL
         self.Us_, self.Vs_, self.V_ = self._msdl(X)
@@ -200,7 +174,7 @@ class MSDL(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
-        """Use the model to transform data to the Shared Response subspace
+        """Use the model to transform data to the low-dimentional subspace
 
         Parameters
         ----------
@@ -230,7 +204,21 @@ class MSDL(BaseEstimator, TransformerMixin):
 
         return s
 
-    def _create_laplacian_operator(self, R):
+    @staticmethod
+    def _create_laplacian_operator(R):
+        """ Pre-computes the 3D-Laplacian operator
+
+        Parameters
+        ----------
+        R : list of 2D arrays, element i has shape=[voxels, 3]
+            Each row in the list contains the scanner coordinate of each voxel
+            of fMRI data of all subjects.
+
+        Returns
+        -------
+        2D sparse array, shape=[voxels,voxels]
+            The laplacian matrix.
+        """
         nmax = R.max(axis=0)+1
         voxels = R.shape[0]
         cube = -np.ones((nmax))
@@ -248,12 +236,12 @@ class MSDL(BaseEstimator, TransformerMixin):
                 if np.any(positions[i,:] < 0) or np.any(positions[i,:]>= nmax)\
                     or cube[positions[i,0],positions[i,1],positions[i,2]]<0:
                     continue
-                data[total_values+offset] = 1.0
+                data[total_values+offset] = -1.0
                 row_ind[total_values+offset] = v
                 col_ind[total_values+offset] = cube[positions[i,0],positions[i,1],positions[i,2]]
                 offset += 1
 
-            data[total_values+offset] = -offset
+            data[total_values+offset] = offset
             row_ind[total_values+offset] = v
             col_ind[total_values+offset] = v
             total_values += offset+1
@@ -310,43 +298,73 @@ class MSDL(BaseEstimator, TransformerMixin):
         return objective
 
     def _update_us(self, data, Vs, Us):
+        """Dictionary (loadings) update for a subject
+
+        Parameters
+        ----------
+        data : array, shape=[voxels, samples]
+            The fMRI data of subject s.
+
+        Vs : array, shape=[voxels, factors]
+            The spatial map :math:`V_s` for subject s.
+
+        Us : array, shape=[samples, factors]
+            The current loadings :math:`U_s` for subject s.
+
+        Returns
+        -------
+        Us : array of shape=[samples, factors]
+            The updated loadings :math:`U_s` for subject s.
+        """
         factors = Vs.shape[1]
-        #TODO: we should make sure that we use the updated atoms to update the dictionary
-        # Need to Check Online dictionary learning work.
-        v_2_squared = np.sum(Vs**2, axis=0)
+        A = Vs.T.dot(Vs)
+        B = data.T.dot(Vs)
         for l in range(factors):
-            #TODO: there was an error in the original paper
-            #TODO: we need to check with the original OnlineDL paper from Julien Mairal
-            Us[:,l] += (data.T.dot(Vs[:,l]) - Us.dot(Vs.T.dot(Vs[:,l])))/v_2_squared[l]
+            dir = Us[:, l] + (B[:,l] - Us.dot(A[:, l])) / A[l,l]
+            Us[:, l] = dir / np.amax((np.linalg.norm(dir, ord=2), 1.0))
         return Us
 
     def _update_vs(self, data, V, Us):
+        """ Spatial map update
+
+        Parameters
+        ----------
+        data : array, shape=[voxels, samples]
+            The fMRI data of subject s.
+
+        V : array, shape=[voxels, factors]
+            The spatial map template :math:`V`.
+
+        Us : array, shape=[samples, factors]
+            The current loadings :math:`U_s` for subject s.
+
+        Returns
+        -------
+        Vsi : array of shape=[voxels, factors]
+            The updated spatial map :math:`V_s` for subject s.
+        """
         factors = self.factors
         A = Us.T.dot(Us) + self.mu * np.eye(factors)
         Vsi = V + np.linalg.solve(A, Us.T.dot(data.T - Us.dot(V.T))).T
         return Vsi
 
-    def _update_v(self, data, Vs):
-        subjects = len(data)
-        meanVs = np.zeros(Vs[0].shape)
-        for s in range(subjects):
-            meanVs += Vs[s]
-        #TODO: need to pass the parameters for the proximal operator
-        return self._prox(meanVs/subjects)
-
-    def _shrink(self, v, offset):
+    @staticmethod
+    def _shrink(v, offset):
         """Computes soft shrinkage on the elements of an array
 
         Parameters
         ----------
+
         v : array
             An array with input values
 
         offset : float
             Offset for applying the shrinkage function
 
+
         Returns
         -------
+
         The array after applying the element-wise soft-thresholding function.
         """
         return np.sign(v) * np.max(np.abs(v) - offset, 0)
@@ -356,27 +374,65 @@ class MSDL(BaseEstimator, TransformerMixin):
 
         Parameters
         ----------
+
         v : array, shape=[voxels,]  or [voxels,n]
             One or more column-vectors containing each a volume
 
+
         Returns
         -------
-        v* (shape=same as input) with the proximal operator applied to the
-        input vectors in v
 
+        v_star : (shape=same as input) with the proximal operator applied to the
+        input vectors in v
         """
-        v_star = v
+        v_star = v.copy()
         z = v_star
         tau = 1.0
-        kappa = 0.2/(1 + self.gamma * self.max_singularval_L_)
-        k = 10
+        kappa = self.kappa/(1 + self.gamma * self.max_eigval_L_)
+        obj_fun_prev = 0
         for l in range(self.fista_iter):
             v0 = v_star
-            v_star = self._shrink(z - kappa*(z - v + self.gamma * self._laplacian(z)), kappa*self.gamma)
+            v_star = self._shrink(z - kappa * (z - v + self.gamma * self._laplacian(z)), kappa * self.gamma)
+
+            # Check for convergence
+            obj_fun = np.sum((v_star-v)**2,0) + self.gamma * (np.sum(np.abs(v_star),0) + 0.5 * np.sum(v * self._laplacian(v),0))
+            if l > 0 and obj_fun > obj_fun_prev:
+                return v0
+            obj_fun_prev = obj_fun
+
             tau0 = tau
-            tau = (1 + np.sqrt(1 + 4*tau*tau))/2
+            tau = (1.0 + np.sqrt(1.0 + 4.0*tau0*tau0))/2.0
             z = v_star + (tau0-1)/tau*(v_star - v0)
         return v_star
+
+    def _update_v(self, data, Vs):
+        """ Spatial map template update
+
+        Parameters
+        ----------
+
+        data : list of 2D arrays, element i has shape=[voxels, samples]
+            Each element in the list contains the fMRI data of one subject.
+
+        Vs : list of 2D arrays, element i has shape=[voxels, factors]
+            The spatial maps :math:`V_s` for each subject.
+
+
+        Returns
+        -------
+
+        V : 2D array, shape=[voxels, factors]
+            The updated spatial map template :math:`V`.
+        """
+        subjects = len(data)
+        meanVs = np.zeros(Vs[0].shape)
+        for s in range(subjects):
+            meanVs += Vs[s]
+        meanVs /= subjects
+        V = np.zeros(Vs[0].shape)
+        for l in range(Vs[0].shape[1]):
+            V[:,l] = self._prox(meanVs[:,l])
+        return V
 
     def _msdl(self, data):
         """Expectation-Maximization algorithm for fitting the probabilistic SRM.
@@ -400,23 +456,18 @@ class MSDL(BaseEstimator, TransformerMixin):
         V  : array, shape=[factors, samples]
             The spatial map template.
         """
-
         subjects = len(data)
-        print(subjects)
         np.random.seed(self.rand_seed)
 
         # Initialization step: initialize the outputs.
         Vs = [None] * subjects
         Us = [None] * subjects
 
-        V = _init_template(data, self.factors)
+        V = self._init_template(data, self.factors)
         print(V.shape)
         print(data[0].shape)
 
         Vu, Vsig, Vv = np.linalg.svd(V, full_matrices=False)
-        print(Vu.shape)
-        print(Vsig.shape)
-        print(Vv.shape)
         for i in range(subjects):
             Vs[i] = V.copy()
             Us[i] = data[i].T.dot(Vu).dot(np.diag(Vsig / (Vsig**2)).dot(Vv))
@@ -430,16 +481,21 @@ class MSDL(BaseEstimator, TransformerMixin):
         # Main loop of the algorithm
         for iteration in range(self.n_iter):
             logger.info('Iteration %d' % (iteration + 1))
-            print(iteration)
-            print(self._objective_function(data, Us, Vs, V))
+
+            self.gamma = self.lam/self.mu/subjects
+            logger.info('Gamma set to %f' % self.gamma)
 
             # Update each subject's decomposition:
             for i in range(subjects):
+                logger.info('Subject %d' % (i))
                 Us[i] = self._update_us(data[i], Vs[i], Us[i])
+                logger.info('After Us update %d' % self._objective_function(data, Us, Vs, V))
                 Vs[i] = self._update_vs(data[i], V, Us[i])
+                logger.info('After Vs update %d' %self._objective_function(data, Us, Vs, V))
 
             # Update the spatial maps template:
             V = self._update_v(data, Vs)
+            logger.info('After V update %d' %self._objective_function(data, Us, Vs, V))
 
             if logger.isEnabledFor(logging.INFO):
                 # Calculate the current objective function value
@@ -447,3 +503,42 @@ class MSDL(BaseEstimator, TransformerMixin):
                 logger.info('Objective function %f' % objective)
 
         return Us, Vs, V
+
+    def _init_template(data, factors):
+        """Initialize the template spatial map (V) for the MSDL with random orthogonal matrices.
+
+        Parameters
+        ----------
+
+        data : list of 2D arrays, element i has shape=[voxels, samples]
+            Each element in the list contains the fMRI data of one subject.
+
+        factors : int
+            The number of factors in the model.
+
+
+        Returns
+        -------
+
+        v : array, shape=[voxels, factors]
+            The initialized template spatial map :math:`V`.
+
+        Note
+        ----
+
+            This function assumes that the numpy random number generator was
+            initialized.
+
+            Not thread safe.
+        """
+        #v = np.empty(data[0].shape[0], factors)
+        #subjects = len(data)
+        V = np.random.random((data[0].shape[0], factors))
+        # Set Wi to a random orthogonal voxels by features matrix
+        # for subject in range(subjects):
+        #     voxels[subject] = data[subject].shape[0]
+        #     rnd_matrix = np.random.random((voxels[subject], features))
+        #     q, r = np.linalg.qr(rnd_matrix)
+        #     w.append(q)
+
+        return V
