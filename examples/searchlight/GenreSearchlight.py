bb51058d1f119401dfba61ff27491ba7b24af569
jamalw <jamalw@princeton.edu>
2017-04-19 15:21:36 -0400

    all comments addressed

--- examples/searchlight/GenreSearchlight.py
+++ examples/searchlight/GenreSearchlight.py
@@ -1,7 +1,7 @@
 import numpy as np
 from nilearn.image import load_img
 import sys
-from brainiak.brainiak.searchlight.searchlight import Searchlight
+from brainiak.searchlight.searchlight import Searchlight
 from scipy import stats
 from scipy.sparse import random
 
@@ -14,7 +14,7 @@ classical = np.random.rand(2600)
 jazz = np.random.rand(2600)
 d1_reshape = np.reshape(data1_rand,(91*109*91,16))
 d2_reshape = np.reshape(data2_rand,(91*109*91,16))
-a1 = load_img(datadir + 'a1plus_2mm.nii')
+a1 = load_img('a1plus_2mm.nii')
 a1_vec = np.reshape(a1.get_data(),(91*109*91))
 a1_idx = np.nonzero(a1_vec)
 for i in range(8):
@@ -32,7 +32,7 @@ data2 = stats.zscore(np.reshape(data2,(91*109*91,16)))
 data2 = np.reshape(data2,(91,109,91,16))
 
 # Load mask 
-mask_img = load_img(datadir + 'MNI152_T1_2mm_brain_mask.nii')
+mask_img = load_img('MNI152_T1_2mm_brain_mask.nii')
 mask_img = mask_img.get_data()
 
 # Definte function that takes the difference between within vs. between genre comparisons
@@ -69,7 +69,7 @@ import matplotlib.pyplot as plt
 for (cnt, img) in enumerate(global_outputs):
   plt.imshow(img,vmin=minval,vmax=maxval)
   plt.colorbar()
-  plt.savefig(datadir + 'searchlight_images/' + 'img' + str(cnt) + '.png')
+  plt.savefig('searchlight_images/' + 'img' + str(cnt) + '.png')
   plt.clf()
 
 
