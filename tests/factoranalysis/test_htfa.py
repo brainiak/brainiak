dd645c682c9906a9802b09536846f19dcf0d0eea
Xia Zhu <xiazhu@users.noreply.github.com>
2017-08-14 17:14:32 -0700

    make n_subj required (#260)
    * make n_subj required
    
    * update test file
    
    * update docstring
    
    * update test
    
    Fixes #259.

--- tests/factoranalysis/test_htfa.py
+++ tests/factoranalysis/test_htfa.py
@@ -18,7 +18,7 @@ def test_R():
     from brainiak.factoranalysis.htfa import HTFA
     with pytest.raises(TypeError) as excinfo:
         HTFA()
-    assert "missing 1 required positional argument" in str(excinfo.value)
+    assert "missing 2 required positional arguments" in str(excinfo.value)
 
 
 def test_X():
@@ -40,6 +40,7 @@ def test_X():
 
     htfa = HTFA(
         K,
+        n_subj=n_subj,
         max_global_iter=max_global_iter,
         max_local_iter=max_local_iter,
         max_voxel=max_voxel,
