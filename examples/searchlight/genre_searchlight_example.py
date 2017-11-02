993ad9fe72fe0b32681a114315ba768199ca23a2
Michael Anderson <michael.j.anderson@intel.com>
2017-11-02 15:04:24 -0700

    searchlight: Make genre_searchlight distributed (#287)
    Note that this example triggers a known issue in Matplotlib 2.1.0 which is fixed in the master branch of Matplotlib:
    https://github.com/matplotlib/matplotlib/issues/9280

--- examples/searchlight/genre_searchlight_example.py
+++ examples/searchlight/genre_searchlight_example.py
@@ -1,6 +1,8 @@
 # The following code is designed to perform a searchlight at every voxel in the brain looking at the difference in pattern similarity between musical genres (i.e. classical and jazz). In the study where the data was obtained, subjects were required to listen to a set of 16 songs twice (two runs) in an fMRI scanner. The 16 songs consisted of 8 jazz songs and 8 classical songs. The goal of this searchlight is to find voxels that seem to represent distinct information about these different musical genres. Presumably, these voxels would be found in the auditory cortex which happens to be the most organized system in the brain for processing sound information. 
 
 import numpy as np
+import time
+from mpi4py import MPI
 from nilearn.image import load_img
 import sys
 from brainiak.searchlight.searchlight import Searchlight
@@ -8,29 +10,39 @@ from scipy import stats
 from scipy.sparse import random
 import os
 
-# Generate random data
-data1_rand = np.random.rand(91,109,91,16)
-data2_rand = np.random.rand(91,109,91,16)
-classical = np.random.rand(2600)
-jazz = np.random.rand(2600)
-d1_reshape = np.reshape(data1_rand,(91*109*91,16))
-d2_reshape = np.reshape(data2_rand,(91*109*91,16))
-a1 = load_img('a1plus_2mm.nii.gz')
-a1_vec = np.reshape(a1.get_data(),(91*109*91))
-a1_idx = np.nonzero(a1_vec)
-for i in range(8):
-    d1_reshape[a1_idx[0],i] += classical
-    d1_reshape[a1_idx[0],i+8] += jazz
-    d2_reshape[a1_idx[0],i] += classical
-    d2_reshape[a1_idx[0],i+8] += jazz
-data1 = np.reshape(d1_reshape,(91,109,91,16))
-data2 = np.reshape(d2_reshape,(91,109,91,16))
+# MPI variables
+comm = MPI.COMM_WORLD
+rank = comm.rank
+size = comm.size
 
-# Flatten data, then zscore data, then reshape data back into MNI coordinate space
-data1 = stats.zscore(np.reshape(data1,(91*109*91,16)))
-data1 = np.reshape(data1,(91,109,91,16))
-data2 = stats.zscore(np.reshape(data2,(91*109*91,16)))
-data2 = np.reshape(data2,(91,109,91,16))
+# Generate random data
+if rank == 0:
+    np.random.seed(0)
+    data1_rand = np.random.rand(91,109,91,16)
+    data2_rand = np.random.rand(91,109,91,16)
+    classical = np.random.rand(2600)
+    jazz = np.random.rand(2600)
+    d1_reshape = np.reshape(data1_rand,(91*109*91,16))
+    d2_reshape = np.reshape(data2_rand,(91*109*91,16))
+    a1 = load_img('a1plus_2mm.nii.gz')
+    a1_vec = np.reshape(a1.get_data(),(91*109*91))
+    a1_idx = np.nonzero(a1_vec)
+    for i in range(8):
+        d1_reshape[a1_idx[0],i] += classical
+        d1_reshape[a1_idx[0],i+8] += jazz
+        d2_reshape[a1_idx[0],i] += classical
+        d2_reshape[a1_idx[0],i+8] += jazz
+    data1 = np.reshape(d1_reshape,(91,109,91,16))
+    data2 = np.reshape(d2_reshape,(91,109,91,16))
+
+    # Flatten data, then zscore data, then reshape data back into MNI coordinate space
+    data1 = stats.zscore(np.reshape(data1,(91*109*91,16)))
+    data1 = np.reshape(data1,(91,109,91,16))
+    data2 = stats.zscore(np.reshape(data2,(91*109*91,16)))
+    data2 = np.reshape(data2,(91,109,91,16))
+else:
+    data1 = None
+    data2 = None
 
 # Load mask 
 mask_img = load_img('MNI152_T1_2mm_brain_mask.nii')
@@ -53,28 +65,37 @@ def corr2_coeff(AB,msk,myrad,bcast_var):
     diff = within_genre - between_genre
     return diff
 
+comm.Barrier()
+begin_time = time.time()
+comm.Barrier()
+
 # Create and run searchlight
 sl = Searchlight(sl_rad=1,max_blk_edge=5)
 sl.distribute([data1,data2],mask_img)
 sl.broadcast(None)
-print('Running Searchlight...')
 global_outputs = sl.run_searchlight(corr2_coeff)
 
-# Plot searchlight results
-maxval = np.max(global_outputs[np.not_equal(global_outputs,None)])
-minval = np.min(global_outputs[np.not_equal(global_outputs,None)])
-global_outputs = np.array(global_outputs, dtype=np.float)
-print(global_outputs)
+comm.Barrier()
+end_time = time.time()
+comm.Barrier()
 
-# Save searchlight images
-out_dir = "searchlight_images"
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
-import matplotlib.pyplot as plt
-for (cnt, img) in enumerate(global_outputs):
-    plt.imshow(img,vmin=minval,vmax=maxval)
-    plt.colorbar()
-    plt.savefig('searchlight_images/' + 'img' + str(cnt) + '.png')
-    plt.clf()
+# Plot searchlight results
+if rank == 0:
+    print('Searchlight Done: ', end_time - begin_time)
+    maxval = np.max(global_outputs[np.not_equal(global_outputs,None)])
+    minval = np.min(global_outputs[np.not_equal(global_outputs,None)])
+    global_outputs = np.array(global_outputs, dtype=np.float)
+    print(global_outputs)
+
+    # Save searchlight images
+    out_dir = "searchlight_images"
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    import matplotlib.pyplot as plt
+    for (cnt, img) in enumerate(global_outputs):
+        plt.imshow(img,vmin=minval,vmax=maxval)
+        plt.colorbar()
+        plt.savefig('searchlight_images/' + 'img' + str(cnt) + '.png')
+        plt.clf()
 
 
