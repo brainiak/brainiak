95d75c11ca9dac197b44469e0eca34988d40249b
Daniel Suo <danielsuo@gmail.com>
2017-12-16 13:29:46 -0500

    Fix pytest path

--- run-tests.sh
+++ run-tests.sh
@@ -40,7 +40,7 @@ fi
 
 if [ ${dist_mode:-default} = "--bdist-mode" ]
 then
-   $mpi_command -n 2 pytest
+  $mpi_command -n 2 $(which pytest)
 
 # Only run coverage for editable or source distribution
 else
