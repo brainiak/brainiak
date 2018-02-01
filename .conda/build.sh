28fa427b43c32eba1195c70a99be48f837361db4
Daniel Suo <danielsuo@gmail.com>
2018-02-01 17:37:03 -0500

    Add conda packaging

---
+++ .conda/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  export MACOSX_DEPLOYMENT_TARGET=10.9
+fi
+
+export CC=$(which clang)
+export CXX=$(which clang++)
+
+# Install pymanopt via pip because there isn't a conda package
+$PYTHON -m pip install pymanopt
+
+# NOTE: This is the recommended way to install packages
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
