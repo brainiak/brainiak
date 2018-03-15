b69c671e377352c7a424d8d24ad1687a21397f53
takua624 <takualiu@gmail.com>
2018-03-15 10:59:11 -0400

    revise isfc
    use p_from_null_mem_saving function to save memory
    add a parameter to let the user decide whether to cast all data into float32 (or even float16)

--- brainiak/isfc.py
+++ brainiak/isfc.py
@@ -32,7 +32,7 @@ comprehension. Nat Commun 7.
 from brainiak.fcma.util import compute_correlation
 import numpy as np
 from scipy import stats
-from .utils.utils import phase_randomize, p_from_null
+from .utils.utils import phase_randomize, p_from_null, p_from_null_mem_saving
 
 
 def isc(D, collapse_subj=True, return_p=False, num_perm=1000,
@@ -110,47 +110,38 @@ def isc(D, collapse_subj=True, return_p=False, num_perm=1000,
 
 
 def isfc(D, collapse_subj=True, return_p=False,
-         num_perm=1000, two_sided=False, random_state=0):
+         num_perm=1000, two_sided=False, random_state=0, float_type=np.float64): # add a parameter
     """Intersubject functional correlation
-
     Computes the correlation between the timecoure of each voxel in each
     subject with the average of all other subjects' timecourses in *all*
     voxels. By default the result is averaged across subjects, unless
     collapse_subj is set to False. A null distribution can optionally be
     computed using phase randomization, to compute a p value for each voxel-to-
     voxel correlation.
-
     Uses the high performance compute_correlation routine from fcma.util
-
     Parameters
     ----------
     D : voxel by time by subject ndarray
         fMRI data for which to compute ISFC
-
     collapse_subj : bool, default:True
         Whether to average across subjects before returning result
-
     return_p : bool, default:False
         Whether to use phase randomization to compute a p value for each voxel
-
     num_perm : int, default:1000
         Number of null samples to use for computing p values
-
     two_sided : bool, default:False
         Whether the p value should be one-sided (testing only for being
         above the null) or two-sided (testing for both significantly positive
         and significantly negative values)
-
     random_state : RandomState or an int seed (0 by default)
         A random number generator instance to define the state of the
         random permutations generator.
-
+	float_type : either float16, float32, or float64, depending on the requiremed precision and available memory in the system
     Returns
     -------
     ISFC : voxel by voxel ndarray
         (or voxel by voxel by subject ndarray, if collapse_subj=False)
         pearson correlation between all pairs of voxels, across subjects
-
     p : ndarray the same shape as ISC (if return_p = True)
         p values for each ISC value under the null distribution
     """
@@ -160,21 +151,34 @@ def isfc(D, collapse_subj=True, return_p=False,
 
     if return_p:
         n_perm = num_perm
+		max_null = np.empty(num_perm).astype(float_type) # added
+		min_null = np.empty(num_perm).astype(float_type) # added
     else:
         n_perm = 0
-
-    ISFC = np.zeros((n_vox, n_vox, n_subj, n_perm + 1))
+	
+	
+    #ISFC = np.zeros((n_vox, n_vox, n_subj, n_perm + 1)) # deleted
+	ISFC = np.zeros((n_vox, n_vox, n_subj)).astype(float_type) # added
 
     for p in range(n_perm + 1):
         # Loop across choice of leave-one-out subject
         for loo_subj in range(D.shape[2]):
             group = np.mean(D[:, :, np.arange(n_subj) != loo_subj], axis=2)
             subj = D[:, :, loo_subj]
-            ISFC[:, :, loo_subj, p] = compute_correlation(group, subj)
-
-            # Symmetrize matrix
-            ISFC[:, :, loo_subj, p] = (ISFC[:, :, loo_subj, p] +
-                                       ISFC[:, :, loo_subj, p].T) / 2
+            #ISFC[:, :, loo_subj, p] = compute_correlation(group, subj) # deleted
+			tmp_ISFC = compute_correlation(group, subj).astype(float_type) # added
+			# Symmetrize matrix
+			tmp_ISFC = (tmp_ISFC+tmp_ISFC.T)/2 # added
+				
+			#ISFC[:, :, loo_subj, p] = (ISFC[:, :, loo_subj, p] +
+								   #ISFC[:, :, loo_subj, p].T) / 2 # deleted
+			if p==0:
+				ISFC[:, :, loo_subj] = tmp_ISFC # added
+			else:
+				leading_dims = tuple(np.arange(tmp_ISFC.ndim)) # added
+				max_null[p-1]=np.max(tmp_ISFC, axis=leading_dims) # added
+				min_null[p-1]=np.min(tmp_ISFC, axis=leading_dims) # added
+				
 
         # Randomize phases of D to create next null dataset
         D = phase_randomize(D, random_state)
@@ -183,7 +187,10 @@ def isfc(D, collapse_subj=True, return_p=False,
         ISFC = np.mean(ISFC, axis=2)
 
     if return_p:
-        p = p_from_null(ISFC, two_sided)
-        return ISFC[..., 0], p
+        # p = p_from_null(ISFC, two_sided) # deleted
+        # return ISFC[..., 0], p # deleted
+		p = p_from_null_mem_saving(ISFC, two_sided, max_null, min_null) # added
+		return ISFC, p # added
     else:
-        return ISFC[..., 0]
+        # return ISFC[..., 0] deleted
+		return ISFC # added
