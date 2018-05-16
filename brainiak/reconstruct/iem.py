9755bda39d9c0fef3a1a9952226e7547a36ca4ea
David Huberdeau <david@Davids-MacBook-Pro.local>
2018-05-16 19:26:38 -0400

    initial commit attempt4

--- brainiak/reconstruct/iem.py
+++ brainiak/reconstruct/iem.py
@@ -11,17 +11,15 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""
-
-    Inverted Encoding Model (IEM)
+"""Inverted Encoding Model (IEM)
 
     The implementation is roughly based on the following publications:
 
-    .. [Kok2013] "1.Kok, P., Brouwer, G. J., Gerven, M. A. J. van &
+    [Kok2013] "1.Kok, P., Brouwer, G. J., Gerven, M. A. J. van &
     Lange, F. P. de. Prior Expectations Bias Sensory Representations
     in Visual Cortex. J. Neurosci. 33, 16275–16284 (2013).
 
-    .. [Bouwer2009] "1.Brouwer, G. J. & Heeger, D. J.
+    [Bouwer2009] "1.Brouwer, G. J. & Heeger, D. J.
     Decoding and Reconstructing Color from Responses in Human Visual
     Cortex. J. Neurosci. 29, 13992–14003 (2009).
 
