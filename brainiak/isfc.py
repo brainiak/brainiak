511afd2794d1049ae81737cacc8ee47d84f81137
Sam Nastase <sam.nastase@gmail.com>
2018-10-02 18:56:34 -0400

    New phase randomization test, both pairwise and leave-one-out

--- brainiak/isfc.py
+++ brainiak/isfc.py
@@ -33,6 +33,7 @@ comprehension. Nat Commun 7.
 import numpy as np
 from scipy.spatial.distance import squareform
 from scipy.stats import pearsonr, zscore
+from scipy.fftpack import fft, ifft
 import itertools as it
 
 def isc(data, pairwise=False, summary_statistic=None, verbose=True):
@@ -465,7 +466,7 @@ def permutation_isc(iscs, group_assignment=None, pairwise=False,
     row/column order of the subject-by-subject square ISC matrix even though the
     input ISCs should be supplied as the vectorized upper triangle of the square
     ISC matrix. Returns the observed ISC and permutation-based p-value (two-tailed
-    test). Optionall  returns the permutation distribution of summary statistics.
+    test). Optionally returns the permutation distribution of summary statistics.
     According to Chen et al., 2016, this is the preferred nonparametric approach
     for controlling false positive rates (FPR) for two-sample tests. This approach
     may yield inflated FPRs for one-sample tests.
@@ -758,11 +759,19 @@ def timeshift_isc(data, pairwise=False, summary_statistic=np.median,
     
     """Circular time-shift randomization for one-sample ISC test
     
-    For a single voxel/ROI, take in response time series for multiple
-    subjects and apply a random temporal shift interval to each subject
-    prior to computing ISCs. Input should be list or dictionary of
-    n_samples x n_voxels time series data where each item in the list
-    or value in the dictionary corresponds to one subject's data.
+    For each voxel or ROI, compute the actual ISC and p-values
+    from a null distribution of ISCs where response time series
+    are first circularly shifted by random intervals. If pairwise,
+    apply time-shift randomization to each subjects and compute pairwise
+    ISCs. If leave-one-out approach is used (pairwise=False), apply
+    the random time-shift to only the left-out subject in each iteration
+    of the leave-one-out procedure. Input data should be a list where
+    each item is a time-points by voxels ndarray for a given subject.
+    Multiple input ndarrays must be the same shape. If a single ndarray is
+    supplied, the last dimension is assumed to correspond to subjects.
+    Returns the observed ISC and p-values (two-tailed test). Optionally
+    returns the null distribution of ISCs computed on randomly time-
+    shifted data.
     
     This implementation is based on the following publications:
 
@@ -878,6 +887,187 @@ def timeshift_isc(data, pairwise=False, summary_statistic=np.median,
                 loo_isc = isc(np.dstack((shifted_subject, nonshifted_mean)), pairwise=False,
                               summary_statistic=None, verbose=False)
                 shifted_isc.append(loo_isc)
+                
+            # Get summary statistics across left-out subjects
+            if summary_statistic == np.mean:
+                shifted_isc = np.tanh(np.mean(np.arctanh(np.dstack(shifted_isc)), axis=2))
+            elif summary_statistic == np.median:
+                shifted_isc = np.median(np.dstack(shifted_isc), axis=2)
+                
+        distribution.append(shifted_isc)
+        
+        # Update random state for next iteration
+        random_state = np.random.RandomState(prng.randint(0, 2**32 - 1))
+        
+    # Convert distribution to numpy array
+    distribution = np.vstack(distribution)
+    assert distribution.shape == (n_shifts, n_voxels)
+
+    # Get p-value for actual median from shifted distribution
+    p = ((np.sum(np.abs(distribution) >= np.abs(observed), axis=0) + 1) /
+          float((len(distribution) + 1)))[np.newaxis, :]
+    
+    if return_distribution:
+        return observed, p, distribution
+    elif not return_distribution:
+        return observed, p
+
+    
+def phaseshift_isc(data, pairwise=False, summary_statistic=np.median,
+                   n_shifts=1000, return_distribution=False, random_state=None):
+    
+    """Phase randomization for one-sample ISC test
+    
+    For each voxel or ROI, compute the actual ISC and p-values
+    from a null distribution of ISCs where response time series
+    are phase randomized prior to computing ISC. If pairwise,
+    apply phase randomization to each subject and compute pairwise
+    ISCs. If leave-one-out approach is used (pairwise=False), only
+    apply phase randomization to the left-out subject in each iteration
+    of the leave-one-out procedure. Input data should be a list where
+    each item is a time-points by voxels ndarray for a given subject.
+    Multiple input ndarrays must be the same shape. If a single ndarray is
+    supplied, the last dimension is assumed to correspond to subjects.
+    Returns the observed ISC and p-values (two-tailed test). Optionally
+    returns the null distribution of ISCs computed on phase-randomized
+    data.
+    
+    This implementation is based on the following publications:
+
+    .. [Lerner2011] "Topographic mapping of a hierarchy of temporal
+    receptive windows using a narrated story.", Y. Lerner, C. J. Honey,
+    L. J. Silbert, U. Hasson, 2011, Journal of Neuroscience, 31, 2906-2915.
+
+    .. [Simony2016] "Dynamic reconfiguration of the default mode network
+    during narrative comprehension.", E. Simony, C. J. Honey, J. Chen, O.
+    Lositsky, Y. Yeshurun, A. Wiesel, U. Hasson, 2016, Nature Communications,
+    7, 12141.
+
+    Parameters
+    ----------
+    data : list or dict, time series data for multiple subjects
+        List or dictionary of response time series for multiple subjects
+
+    pairwise : bool, default:False
+        Indicator of pairwise or leave-one-out, should match iscs variable
+
+    summary_statistic : numpy function, default:np.median
+        Summary statistic, either np.median (default) or np.mean
+        
+    n_shifts : int, default:1000
+        Number of randomly shifted samples
+        
+    return_distribution : bool, default:False
+        Optionally return the bootstrap distribution of summary statistics
+        
+    random_state = int, None, or np.random.RandomState, default:None
+        Initial random seed
+
+    Returns
+    -------
+    observed : float, observed ISC (without time-shifting)
+        Actual ISCs
+
+    p : float, p-value
+        p-value based on time-shifting randomization test
+        
+    distribution : ndarray, time-shifts by voxels (optional)
+        Time-shifted null distribution if return_bootstrap=True
+    """
+
+    # Convert list input to 3d and check shapes
+    if type(data) == list:
+        data_shape = data[0].shape
+        for i, d in enumerate(data):
+            if d.shape != data_shape:
+                raise ValueError("All ndarrays in input list "
+                                 "must be the same shape!")
+            if d.ndim == 1:
+                data[i] = d[:, np.newaxis]
+        data = np.dstack(data)
+
+    # Convert input ndarray to 3d and check shape
+    elif type(data) == np.ndarray:
+        if data.ndim == 2:
+            data = data[:, np.newaxis, :]            
+        elif data.ndim == 3:
+            pass
+        else:
+            raise ValueError("Input ndarray should have 2 "
+                             f"or 3 dimensions (got {data.ndim})!")
+
+    # Infer subjects, TRs, voxels and print for user to check
+    n_subjects = data.shape[2]
+    n_TRs = data.shape[0]
+    n_voxels = data.shape[1]
+    
+    # Get actual observed ISC
+    observed = isc(data, pairwise=pairwise, summary_statistic=summary_statistic)
+    
+    # Iterate through randomized shifts to create null distribution
+    distribution = []
+    for i in np.arange(n_shifts):
+        
+        # Random seed to be deterministically re-randomized at each iteration
+        if isinstance(random_state, np.random.RandomState):
+            prng = random_state
+        else:
+            prng = np.random.RandomState(random_state)
+            
+        # Get randomized phase shifts
+        if data.shape[0] % 2 == 0:
+            # Why are we indexing from 1 not zero here? Vector is n_TRs / -1 long?
+            pos_freq = np.arange(1, data.shape[0] // 2)
+            neg_freq = np.arange(data.shape[0] - 1, data.shape[0] // 2, -1)
+        else:
+            pos_freq = np.arange(1, (data.shape[0] - 1) // 2 + 1)
+            neg_freq = np.arange(data.shape[0] - 1, (data.shape[0] - 1) // 2, -1)
+
+        phase_shifts = prng.rand(len(pos_freq), 1, n_subjects) * 2 * np.math.pi
+        
+        # In pairwise approach, apply all shifts then compute pairwise ISCs
+        if pairwise:
+        
+            # Fast Fourier transform along time dimension of data
+            fft_data = fft(data, axis=0)
+
+            # Shift pos and neg frequencies symmetrically, to keep signal real
+            fft_data[pos_freq, :, :] *= np.exp(1j * phase_shifts)
+            fft_data[neg_freq, :, :] *= np.exp(-1j * phase_shifts)
+
+            # Inverse FFT to put data back in time domain for ISC
+            shifted_data = np.real(ifft(fft_data, axis=0))
+
+            # Compute null ISC on shifted data for pairwise approach
+            shifted_isc = isc(shifted_data, pairwise=True,
+                              summary_statistic=summary_statistic, verbose=False)
+        
+        # In leave-one-out, apply shift only to each left-out participant
+        elif not pairwise:
+            
+            # Roll subject axis in phaseshifts for loop
+            phase_shifts = np.rollaxis(phase_shifts, 2, 0)
+            
+            shifted_isc = []
+            for s, shift in enumerate(phase_shifts):
+                
+                # Apply FFT to left-out subject
+                fft_subject = fft(data[:, :, s], axis=0)
+                
+                # Shift pos and neg frequencies symmetrically, to keep signal real
+                fft_subject[pos_freq, :] *= np.exp(1j * shift)
+                fft_subject[neg_freq, :] *= np.exp(-1j * shift)
+
+                # Inverse FFT to put data back in time domain for ISC
+                shifted_subject = np.real(ifft(fft_subject, axis=0))
+
+                # Compute ISC of shifted left-out subject against mean of N-1 subjects
+                nonshifted_mean = np.mean(np.delete(data, s, 2), axis=2)
+                loo_isc = isc(np.dstack((shifted_subject, nonshifted_mean)), pairwise=False,
+                              summary_statistic=None, verbose=False)
+                shifted_isc.append(loo_isc)
+                
+            # Get summary statistics across left-out subjects
             if summary_statistic == np.mean:
                 shifted_isc = np.tanh(np.mean(np.arctanh(np.dstack(shifted_isc)), axis=2))
             elif summary_statistic == np.median:
