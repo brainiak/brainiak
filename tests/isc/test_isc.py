304ad5006c049dc5c9cef568056374f50f18fa26
Sam Nastase <sam.nastase@gmail.com>
2018-11-19 13:08:18 -0500

    Changed module name from isfc to isc, which is more general family of analyses

--- tests/isc/test_isc.py
+++ tests/isc/test_isc.py
@@ -1,6 +1,6 @@
 import numpy as np
 import logging
-from brainiak.isfc import (isc, isfc, bootstrap_isc, permutation_ics,
+from brainiak.isc import (isc, isfc, bootstrap_isc, permutation_ics,
                            timeshift_isc, phaseshift_isc)
 from scipy.spatial.distance import squareform
 
