494e26d559daa67954665b71da97feac4ae2e591
Sam Nastase <sam.nastase@gmail.com>
2019-04-17 19:00:03 -0400

    New array_correlation function in utils

--- brainiak/utils/utils.py
+++ brainiak/utils/utils.py
@@ -28,6 +28,7 @@ Some utility functions that can be used by different algorithms
 """
 
 __all__ = [
+    "array_correlation",
     "center_mass_exp",
     "concatenate_not_none",
     "cov2corr",
@@ -909,3 +910,64 @@ def _check_timeseries_input(data):
                     n_subjects, n_TRs, n_voxels))
 
     return data, n_TRs, n_voxels, n_subjects
+
+
+def array_correlation(x, y, axis=0):
+
+    """Column- or row-wise Pearson correlation between two arrays
+
+    Computes sample Pearson correlation between two 1D or 2D arrays (e.g.,
+    two n_TRs by n_voxels arrays). For 2D arrays, computes correlation
+    between each corresponding column (axis=0) or row (axis=1) where axis
+    indexes observations. If axis=0 (default), each column is considered to
+    be a variable and each row is an observation; if axis=1, each row is a
+    variable and each column is an observation (equivalent to transposing
+    the input arrays). Input arrays must be the same shape with corresponding
+    variables and observations. This is intended to be an efficient method
+    for computing correlations between two corresponding arrays with many
+    variables (e.g., many voxels).
+
+    Parameters
+    ----------
+    x : 1D or 2D ndarray
+        Array of observations for one or more variables
+
+    y : 1D or 2D ndarray
+        Array of observations for one or more variables (same shape as x)
+
+    axis : int (0 or 1), default: 0
+        Correlation between columns (axis=0) or rows (axis=1)
+
+    Returns
+    -------
+    r : float or 1D ndarray
+        Pearson correlation values for input variables
+    """
+
+    # Accommodate array-like inputs
+    if not isinstance(x, np.ndarray):
+        x = np.asarray(x)
+    if not isinstance(y, np.ndarray):
+        y = np.asarray(y)
+    
+    # Check that inputs are same shape    
+    if x.shape != y.shape:
+        raise ValueError("Input arrays must be the same shape")
+        
+    # Transpose if axis=1 requested (to avoid broadcasting
+    # issues introduced by switching axis in mean and sum)
+    if axis == 1:
+        x, y = x.T, y.T
+
+    # Center (de-mean) input variables
+    x_demean = x - np.mean(x, axis=0)
+    y_demean = y - np.mean(y, axis=0)
+
+    # Compute summed product of centered variables    
+    numerator = np.sum(x_demean * y_demean, axis=0)
+                       
+    # Compute sum squared error
+    denominator = np.sqrt(np.sum(x_demean ** 2, axis=0) *
+                          np.sum(y_demean ** 2, axis=0))
+    
+    return numerator / denominator
