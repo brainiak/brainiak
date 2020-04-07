dc15451c5eb93f5d6807f4e63209e4c4f759bbdd
CameronTEllis <cellis@princeton.edu>
2020-04-07 18:48:39 -0400

    fixed the resource stream path

--- brainiak/utils/fmrisim.py
+++ brainiak/utils/fmrisim.py
@@ -2217,9 +2217,9 @@ def mask_brain(volume,
     if mask_self is True:
         mask_raw = volume
     elif template_name is None:
-        mask_raw = np.load(resource_stream(
-            fmrisim_real_time_generator.__name__,
-            "sim_parameters/grey_matter_mask.npy"))
+        mask_name = resource_stream(__name__,
+                                 "sim_parameters/grey_matter_mask.npy")
+        mask_raw = np.load(mask_name)
     else:
         mask_raw = np.load(template_name)
 
