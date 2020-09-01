4e0397aacc0392984902eec1abb52de65bdf4d39
Michael Shvartsman <mshvartsman@users.noreply.github.com>
2020-08-31 22:30:05 -0700

    fix conda build for travis

--- .conda/build.sh
+++ .conda/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Install pymanopt via pip because there isn't a conda package
+# Install pymanopt and tensorflow_probability via pip because there isn't a conda package
 PIP_NO_INDEX=False $PYTHON -m pip install pymanopt
+PIP_NO_INDEX=False $PYTHON -m pip install tensorflow_probability
 
 # NOTE: This is the recommended way to install packages
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
