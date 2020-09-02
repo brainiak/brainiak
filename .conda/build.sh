f9f595eedcd04ce060f358af2bd8575d7f06b11f
Michael Shvartsman <mshvartsman@users.noreply.github.com>
2020-09-01 20:25:17 -0700

    pull in TF2.3 from PyPI since conda only has 2.2

--- .conda/build.sh
+++ .conda/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Install pymanopt and tensorflow_probability via pip because there isn't a conda package
+# Install pymanopt and tensorflow_probability via pip because there isn't a conda 
+# package, and install tensorflow 2.3 from pip because tensorflow_probability
+# requires it and latest in conda is 2.2. 
+PIP_NO_INDEX=False $PYTHON -m pip install tensorflow>=2.3
 PIP_NO_INDEX=False $PYTHON -m pip install pymanopt
 PIP_NO_INDEX=False $PYTHON -m pip install tensorflow_probability
+
 
 # NOTE: This is the recommended way to install packages
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
