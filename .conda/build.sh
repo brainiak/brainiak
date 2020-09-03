9e60f23454ae0ce4e7de3f8b4ea23f0f1823ff55
Michael Shvartsman <mshvartsman@users.noreply.github.com>
2020-09-02 20:52:15 -0700

    escape

--- .conda/build.sh
+++ .conda/build.sh
@@ -3,7 +3,7 @@
 # Install pymanopt and tensorflow_probability via pip because there isn't a conda 
 # package, and install tensorflow 2.3 from pip because tensorflow_probability
 # requires it and latest in conda is 2.2. 
-PIP_NO_INDEX=False $PYTHON -m pip install tensorflow>=2.3
+PIP_NO_INDEX=False $PYTHON -m pip install tensorflow\>=2.3
 PIP_NO_INDEX=False $PYTHON -m pip install pymanopt
 PIP_NO_INDEX=False $PYTHON -m pip install tensorflow_probability
 
