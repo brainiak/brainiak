bcc2e87fe04ce697e8aa18452283860cf30b65e9
David Turner <dmturner@princeton.edu>
2022-09-19 15:44:59 -0400

    Pin pymanopt install in .conda/buiild.sh like in install_requires

--- .conda/build.sh
+++ .conda/build.sh
@@ -3,7 +3,7 @@
 # Install from PyPI because there is no current conda package for the
 # following. Explicitly install dependencies with no conda package as well
 # because otherwise conda-build does not include them in the output package.
-PIP_NO_INDEX=False $PYTHON -m pip install pymanopt
+PIP_NO_INDEX=False $PYTHON -m pip install pymanopt<=0.2.5
 
 # NOTE: This is the recommended way to install packages
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
