3344931b9a0c08532dae294d5ef2200a55abbda4
David Turner <davidt0x@gmail.com>
2024-10-02 18:52:26 -0400

    Attempt to fix conda build

--- .conda/build.sh
+++ .conda/build.sh
@@ -3,7 +3,6 @@
 # Install from PyPI because there is no current conda package for the
 # following. Explicitly install dependencies with no conda package as well
 # because otherwise conda-build does not include them in the output package.
-PIP_NO_INDEX=False $PYTHON -m pip install "pymanopt<=0.2.5"
+PIP_NO_INDEX=False $PYTHON -m pip install "pymanopt"
 
-# NOTE: This is the recommended way to install packages
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation
