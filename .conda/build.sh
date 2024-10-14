6e191de9e9268c3b4285b80570bc59eadb2484a4
Dave Turner <davidt0x@gmail.com>
2024-10-14 13:44:23 -0400

    Try to get version from setuptools_scm

--- .conda/build.sh
+++ .conda/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Install from PyPI because there is no current conda package for the
-# following. Explicitly install dependencies with no conda package as well
-# because otherwise conda-build does not include them in the output package.
-PIP_NO_INDEX=False $PYTHON -m pip install "pymanopt"
-
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
+## Install from PyPI because there is no current conda package for the
+## following. Explicitly install dependencies with no conda package as well
+## because otherwise conda-build does not include them in the output package.
+#PIP_NO_INDEX=False $PYTHON -m pip install "pymanopt"
+#
+#$PYTHON -m pip install . -vv --no-deps --no-build-isolation
